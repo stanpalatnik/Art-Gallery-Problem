340fa19a9b7569e629cfd8c94a54888c7d10afe5
stanpalatnik <yankees27an@gmail.com>
2012-04-08 13:52:34 -0400

    Working to bind points changes to the UI.

--- GeometryTest/Models/Polygon.cs
+++ GeometryTest/Models/Polygon.cs
@@ -10,7 +10,7 @@ using System.Windows;
 
 namespace GeometryTest
 {
-    class Polygon : System.ComponentModel.INotifyPropertyChanged, System.Dynamic.IDynamicMetaObjectProvider
+    class Polygon : System.ComponentModel.INotifyPropertyChanged
     {
 
         private static readonly Polygon instance = new Polygon();
@@ -90,6 +90,26 @@ namespace GeometryTest
             vertices.Clear();
             //reset button states
 
+        }
+
+        public class PointCollectionConverter2 : System.Windows.Data.IValueConverter
+        {
+            public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                if (value.GetType() == typeof(ObservableCollection<ColoredPoint>) && targetType == typeof(System.Windows.Media.PointCollection))
+                {
+                    var pointCollection = new System.Windows.Media.PointCollection();
+                    foreach (var coloredPoint in value as ObservableCollection<ColoredPoint>)
+                        pointCollection.Add(coloredPoint.point);
+                    return pointCollection;
+                }
+                return null;
+            }
+
+            public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                return null;
+            }
         }
     }
 }
