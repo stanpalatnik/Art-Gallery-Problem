4503d2f406bc9347fa632927ed17db113a608cdc
stanpalatnik <yankees27an@gmail.com>
2012-04-21 15:13:42 -0400

    Worked on DiagonalSet

--- GeometryTest/Models/DiagonalSet.cs
+++ GeometryTest/Models/DiagonalSet.cs
@@ -9,36 +9,33 @@ namespace GeometryTest.Models
 {
     class DiagonalSet
     {
-        Edge[] dSet = new Edge[200];
-        public int size = 0;
-
+        List<Edge> diagonalSet = new List<Edge>();
         public DiagonalSet() { }
         public void addDiagonal(ColoredPoint i, ColoredPoint j, ColoredPoint cutOff)
         {
-            dSet[size] = new Edge(i, j, cutOff);
-            size++;
+            diagonalSet.Add(new Edge(i, j, cutOff));
         }
         public Edge getDiagonal(int i)
         {
-            return dSet[i];
+            return diagonalSet[i];
         }
         public int getSize()
         {
-            return size;
+            return diagonalSet.Count;
         }
         public int isInDiagSet(ColoredPoint a, ColoredPoint b)
         {
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < diagonalSet.Count; i++)
             {
-                if (((dSet[i].getStart().point.X == a.point.X) &&
-                  (dSet[i].getStart().point.Y == a.point.Y) &&
-                  (dSet[i].getEnd().point.X == b.point.X) &&
-                  (dSet[i].getEnd().point.Y == b.point.Y))
+                if (((diagonalSet[i].getStart().point.X == a.point.X) &&
+                  (diagonalSet[i].getStart().point.Y == a.point.Y) &&
+                  (diagonalSet[i].getEnd().point.X == b.point.X) &&
+                  (diagonalSet[i].getEnd().point.Y == b.point.Y))
                   ||
-                  ((dSet[i].getEnd().point.X == a.point.X) &&
-                  (dSet[i].getEnd().point.Y == a.point.Y) &&
-                  (dSet[i].getStart().point.X == b.point.X) &&
-                  (dSet[i].getStart().point.Y == b.point.Y)))
+                  ((diagonalSet[i].getEnd().point.X == a.point.X) &&
+                  (diagonalSet[i].getEnd().point.Y == a.point.Y) &&
+                  (diagonalSet[i].getStart().point.X == b.point.X) &&
+                  (diagonalSet[i].getStart().point.Y == b.point.Y)))
                     return i;
             }
             return -1;
@@ -46,11 +43,10 @@ namespace GeometryTest.Models
         public DiagonalSet merge(DiagonalSet d2)
         {
             int d2size = d2.getSize();
-            int i = size;
 
-            for (int j = 0; j < d2size; j++, i++, size++)
+            for (int j = 0; j < d2size; j++)
             {
-                dSet[size] = d2.getDiagonal(j);
+                diagonalSet.Add(d2.getDiagonal(j));
             }
 
             return this;
