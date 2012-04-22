f401b48f04ff9044622ab0d4387cca0e644a76b0
Kai Sellgren <kaisellgren@gmail.com>
2012-04-22 20:23:37 +0300

    Update GeometryTest/App.xaml.cs

--- GeometryTest/App.xaml.cs
+++ GeometryTest/App.xaml.cs
@@ -13,5 +13,11 @@ namespace GeometryTest
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Resources.Add("polygon", Polygon.Instance);
+        }
     }
 }
